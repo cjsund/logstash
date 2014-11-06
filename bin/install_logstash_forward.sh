77f1cc36bf1b5a4d9ea02b0cd10c96ea53b105ef
yangyong <yangyong@yangyongdeMacBook-Air.local>
2014-11-06 11:38:26 +0800

    添加logstash_forward安装脚本

---
+++ bin/install_logstash_forward.sh
@@ -0,0 +1,12 @@
+#!/bin/evn bash
+#2014-11-06
+
+cp /etc/apt/sources.list /etc/apt/sources.list.date '+%y%m%d%H%M'
+echo 'deb http://packages.elasticsearch.org/logstashforwarder/debian stable main' | tee /etc/apt/sources.list.d/logstashforwarder.list
+apt-get update
+apt-get install logstash-forwarder
+cd /etc/init.d/
+wget https://raw.github.com/elasticsearch/logstash-forwarder/master/logstash-forwarder.init -O logstash-forwarder
+chmod +x logstash-forwarder
+update-rc.d logstash-forwarder defaults
+mkdir -p /etc/pki/tls/certs
