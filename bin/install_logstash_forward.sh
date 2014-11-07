76faac7c1bc216b3ccce834d5fd30ce44112958f
yangyong <cjsund@126.com>
2014-11-07 15:57:44 +0800

    添加logstash配置

--- bin/install_logstash_forward.sh
+++ bin/install_logstash_forward.sh
@@ -2,7 +2,7 @@
 #2014-11-06
 
 set -e
-cp /etc/apt/sources.list /etc/apt/sources.list.date '+%y%m%d%H%M'
+cp /etc/apt/sources.list /etc/apt/sources.list.`date '+%y%m%d%H%M'`
 echo 'deb http://packages.elasticsearch.org/logstashforwarder/debian stable main' | tee /etc/apt/sources.list.d/logstashforwarder.list
 apt-get update
 apt-get -f install
@@ -12,3 +12,23 @@ wget https://raw.github.com/elasticsearch/logstash-forwarder/master/logstash-for
 chmod +x logstash-forwarder
 update-rc.d logstash-forwarder defaults
 mkdir -p /etc/pki/tls/certs
+
+cat > /etc/logstash-forwarder << EOF
+{
+  "network": {
+    "servers": [ "192.168.1.253:5000" ],
+    "timeout": 15,
+    "ssl ca": "/etc/pki/tls/certs/logstash-forwarder.crt"
+  },
+  "files": [
+    {
+      "paths": [
+        "/var/log/syslog",
+        "/var/log/auth.log"
+       ],
+      "fields": { "type": "syslog" }
+    }
+   ]
+}
+
+EOF
