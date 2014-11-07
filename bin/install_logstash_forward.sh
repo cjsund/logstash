3c69c016c417e360969f95890bf96e60ea5df9ef
yangyong <cjsund@126.com>
2014-11-07 15:44:48 +0800

    + set -e

--- bin/install_logstash_forward.sh
+++ bin/install_logstash_forward.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #2014-11-06
 
+set -e
 cp /etc/apt/sources.list /etc/apt/sources.list.date '+%y%m%d%H%M'
 echo 'deb http://packages.elasticsearch.org/logstashforwarder/debian stable main' | tee /etc/apt/sources.list.d/logstashforwarder.list
 apt-get update
-apt-get install -y logstash-forwarder
+apt-get -f install
+apt-get -y install logstash-forwarder
 cd /etc/init.d/
 wget https://raw.github.com/elasticsearch/logstash-forwarder/master/logstash-forwarder.init -O logstash-forwarder
 chmod +x logstash-forwarder
